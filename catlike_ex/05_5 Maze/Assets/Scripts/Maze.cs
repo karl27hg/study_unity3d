e2568d4113e8d57c1bfbdb240f59e713d7d84cd8
han <karl27hg@gmail.com>
2018-01-22 21:53:24 +0900

    05_5 Maze Decorating the Maze

--- catlike_ex/05_5 Maze/Assets/Scripts/Maze.cs
+++ catlike_ex/05_5 Maze/Assets/Scripts/Maze.cs
@@ -14,7 +14,7 @@ public class Maze : MonoBehaviour
 
     // wall
     public MazePassage passagePrefab;
-    public MazeWall wallPrefab;
+    public MazeWall[] wallPrefabs;
 
     public MazeCell GetCell(IntVector2 coordinates)
     {
@@ -116,11 +116,11 @@ public class Maze : MonoBehaviour
 
     private void CreateWall(MazeCell cell, MazeCell otherCell, MazeDirection direction)
     {
-        MazeWall wall = Instantiate(wallPrefab) as MazeWall;
+        MazeWall wall = Instantiate(wallPrefabs[Random.Range(0, wallPrefabs.Length)]) as MazeWall;
         wall.Initialize(cell, otherCell, direction);
         if(otherCell != null)
         {
-            wall = Instantiate(wallPrefab) as MazeWall;
+            wall = Instantiate(wallPrefabs[Random.Range(0, wallPrefabs.Length)]) as MazeWall;
             wall.Initialize(otherCell, cell, direction.GetOpposite());
         }
     }
